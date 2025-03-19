617f4f86aaad49997e382ca0eb0d42c57aaa940c
Jonathan Marcantonio <jmarcant@redhat.com>
2025-03-19 14:09:45 -0400

    Update protos to proposed versions. (check + checkforupdate)
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- src/main/java/org/project_kessel/inventory/client/KesselCheckClient.java
+++ src/main/java/org/project_kessel/inventory/client/KesselCheckClient.java
@@ -2,12 +2,10 @@ package org.project_kessel.inventory.client;
 
 import java.util.logging.Logger;
 
-import org.project_kessel.api.inventory.v1beta1.authz.CheckForCreateRequest;
-import org.project_kessel.api.inventory.v1beta1.authz.CheckForCreateResponse;
 import org.project_kessel.api.inventory.v1beta1.authz.CheckForUpdateRequest;
 import org.project_kessel.api.inventory.v1beta1.authz.CheckForUpdateResponse;
-import org.project_kessel.api.inventory.v1beta1.authz.CheckForViewRequest;
-import org.project_kessel.api.inventory.v1beta1.authz.CheckForViewResponse;
+import org.project_kessel.api.inventory.v1beta1.authz.CheckRequest;
+import org.project_kessel.api.inventory.v1beta1.authz.CheckResponse;
 import org.project_kessel.api.inventory.v1beta1.authz.KesselCheckServiceGrpc;
 import org.project_kessel.clients.KesselClient;
 
@@ -24,19 +22,19 @@ public class KesselCheckClient extends KesselClient<KesselCheckServiceGrpc.Kesse
     }
 
 
-    public CheckForViewResponse CheckForView(CheckForViewRequest request) {
-        return blockingStub.checkForView(request);
+    public CheckResponse Check(CheckRequest request) {
+        return blockingStub.check(request);
     }
 
-    public void CheckForView(CheckForViewRequest request, StreamObserver<CheckForViewResponse> responObserver) {
-        asyncStub.checkForView(request, responObserver);
+    public void Check(CheckRequest request, StreamObserver<CheckResponse> responObserver) {
+        asyncStub.check(request, responObserver);
     }
     
-    public Uni<CheckForViewResponse> CheckForViewUni(CheckForViewRequest request) {
-        final UnicastProcessor<CheckForViewResponse> responseProcessor = UnicastProcessor.create();
-        var streamObserver = new StreamObserver<CheckForViewResponse>() {
+    public Uni<CheckResponse> CheckUni(CheckRequest request) {
+        final UnicastProcessor<CheckResponse> responseProcessor = UnicastProcessor.create();
+        var streamObserver = new StreamObserver<CheckResponse>() {
             @Override
-            public void onNext(CheckForViewResponse response) {
+            public void onNext(CheckResponse response) {
                 responseProcessor.onNext(response);
             }
 
@@ -51,7 +49,7 @@ public class KesselCheckClient extends KesselClient<KesselCheckServiceGrpc.Kesse
             }
         };
         var uni = Uni.createFrom().publisher(responseProcessor);
-        CheckForView(request, streamObserver);
+        Check(request, streamObserver);
         return uni;
     }
 
@@ -84,38 +82,6 @@ public class KesselCheckClient extends KesselClient<KesselCheckServiceGrpc.Kesse
         };
         var uni = Uni.createFrom().publisher(responseProcessor);
         CheckForUpdate(request, streamObserver);
-        return uni;
-    }
-
-
-    public CheckForCreateResponse CheckForCreate(CheckForCreateRequest request) {
-        return blockingStub.checkForCreate(request);
-    }
-
-    public void CheckForCreate(CheckForCreateRequest request, StreamObserver<CheckForCreateResponse> responObserver) {
-        asyncStub.checkForCreate(request, responObserver);
-    }
-    
-    public Uni<CheckForCreateResponse> CheckForCreateUni(CheckForCreateRequest request) {
-        final UnicastProcessor<CheckForCreateResponse> responseProcessor = UnicastProcessor.create();
-        var streamObserver = new StreamObserver<CheckForCreateResponse>() {
-            @Override
-            public void onNext(CheckForCreateResponse response) {
-                responseProcessor.onNext(response);
-            }
-
-            @Override
-            public void onError(Throwable t) {
-                responseProcessor.onError(t);
-            }
-
-            @Override
-            public void onCompleted() {
-                responseProcessor.onComplete();
-            }
-        };
-        var uni = Uni.createFrom().publisher(responseProcessor);
-        CheckForCreate(request, streamObserver);
         return uni;
     }
 }
